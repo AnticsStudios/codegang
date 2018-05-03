0583e47101ee2a86a50d9314b6e8a9ecc9d5e9f0
MASONSLAPPIE\Mason Lee <definitelymason@gmail.com>
2018-05-03 15:28:58 -0400

    test to practice

--- Beta/Assets/_scripts/SimpleScript.cs
+++ Beta/Assets/_scripts/SimpleScript.cs
@@ -7,6 +7,7 @@ public class SimpleScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		//Debug.log("Start");
+		//this is from mason
 	}
 	
 	// Update is called once per frame
