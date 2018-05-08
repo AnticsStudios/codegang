a979f370336ea14f25b60925147557eead4d2634
IMAC\Chris <antics_chris@hotmail.com>
2018-05-08 19:47:55 -0400

    [UI] Added animations for buttons sliding in/out Added settings panel for audio muting / sliding

--- Beta/Assets/_scripts/LoadSceneScript.cs
+++ Beta/Assets/_scripts/LoadSceneScript.cs
@@ -3,12 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadSceneScript : MonoBehaviour {
 
+
+public class LoadSceneScript : MonoBehaviour
+{
+
+    public Animator startButton;
+    public Animator settingsButton;
+    public Animator quitButton;
+    public Animator dialog;
 
     public void StartGame()
     {
         SceneManager.LoadScene("GameScene");
+    }
+
+    public void OpenSettings()
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+        quitButton.SetBool("isHidden", true);
+        dialog.SetBool("isHidden", false);
+    }
+
+    public void CloseSettings()
+    {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        quitButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
     }
 
 }
